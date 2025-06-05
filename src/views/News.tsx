31f3e48b62d40fa8a03d4fcd18baa3ec07b68b39
ReverseChin0 <38972126+ReverseChin0@users.noreply.github.com>
2025-06-05 11:50:54 -0600

    - Made News tab scrollable

--- src/views/News.tsx
+++ src/views/News.tsx
@@ -7,18 +7,20 @@ type newsProps = {
 const News = ( {news} : newsProps ) => {
 
     return (
-      <div className='frame-bg news-frame'>
-        <div>
-            <h1>News</h1>
-            { 
-                news.map((article,index)=>{
-                    const { title,message,date } = article;
-                    return <div className="new-item" key={`article_${index}`}>
-                        <h3>{title}</h3> <h5>{date}</h5>
-                        <p>{message}</p>
-                    </div>
-                }) 
-            }
+      <div className='frame-bg'>
+        <div className="news">
+            <div className="news-scroll">
+                <h1>News</h1>
+                { 
+                    news.map((article,index)=>{
+                        const { title,message,date } = article;
+                        return <div className="new-item" key={`article_${index}`}>
+                            <h3>{title}</h3> <h5>{date}</h5>
+                            <p>{message}</p>
+                        </div>
+                    }) 
+                }
+            </div>
         </div>
       </div>
     )
